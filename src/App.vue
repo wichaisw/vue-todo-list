5deb3825a75ee807d9f4df9b804d3067f73e0471
wichaisw <wichai.sw19@gmail.com>
2020-08-04 21:36:06 +0700

    add event, emit the event to parent component

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,56 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- pass todos object as a prop named todoObj -->
+    <Todos v-bind:todoObj='todos' v-on:del-todo='deleteTodo' />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Todos from './components/Todos';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    Todos,
+  },
+  data() {
+    return {
+      todos: [
+        {
+          id: 1,
+          title: 'Todo One',
+          completed: false,
+        },
+        {
+          id: 2,
+          title: 'Todo Two',
+          completed: true,
+        },
+        {
+          id: 3,
+          title: 'Todo Three',
+          completed: true,
+        },
+      ]
+    }
+  },
+  methods: {
+    deleteTodo(id) {
+      this.todo = this.todo.filter(todo => todo.id !== id);
+    }
+  },
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  *{
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+
+  body {
+    font-family: Arial, Helvetica, sans-serif;
+    line-height: 1.4;
+  }
 </style>
---
+++ src/components/Todos.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <!-- v-bind:key กำหนด key ให้ loop element เหมือน react -->
+    <!-- v-for ใช้ loop array ของ object todoObj -->
+    <div v-bind:key='todo.id' v-for='todo in todoObj'>
+      <!-- <h3>{{todo.title}}</h3> -->
+      <TodoItem v-bind:todo='todo' v-on:del-todo='$emit("del-todo", todo.id)' />
+    </div>
+  </div>
+</template>
+
+<script>
+import TodoItem from './TodoItem';
+
+export default {
+  name: 'Todos',
+  components: {
+    TodoItem,
+  },
+  props: ['todoObj']
+}
+</script>
+
+<style scoped>
+  
+</style>
